e7a3131d06e243ca3c0ef2de1724f70306849d59
Juan Marin Pareja <marinpareja@gmail.com>
2011-06-03 16:15:03 +0200

    Merge index.html to create javascript files

---
+++ js/puzzle-hover.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$("#menu-puzzle-00").hover(
+		function() { $("#text-requirements").css('font-weight', 'bold');},
+		function() { $("#text-requirements").css('font-weight', '400');	}
+	);
+
+	$("#menu-puzzle-01").hover(
+		function() { $("#text-execute").css('font-weight', 'bold');},
+		function() { $("#text-execute").css('font-weight', '400');	}
+	);
+
+	$("#menu-puzzle-10").hover(
+		function() { $("#text-store").css('font-weight', 'bold');},
+		function() { $("#text-store").css('font-weight', '400');	}
+	);
+
+	$("#menu-puzzle-11").hover(
+		function() { $("#text-report").css('font-weight', 'bold');},
+		function() { $("#text-report").css('font-weight', '400');	}
+	);
+});
